5f157d696385be125f9fb51bf0346423b14b8dc8
이중석 <ejoongseok@gmail.com>
2024-04-08 08:34:40 +0900

    Initial commit

---
+++ src/main/java/com/ejs/legacyrefactoringexample/LegacyRefactoringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ejs.legacyrefactoringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyRefactoringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacyRefactoringExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ejs/legacyrefactoringexample/LegacyRefactoringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejs.legacyrefactoringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyRefactoringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
